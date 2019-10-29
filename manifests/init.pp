61d23e1e3b468922d6413549d681e2dd1aa4a53d
Bill Glick <bglick@illinois.edu>
2019-10-29 14:40:29 -0500

    initial release

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+# Install and run ptp time service
+#
+# @summary Install and run ptp time service
+#
+# @example
+#   include ptp
+class ptp (
+  $packages,
+  $enabled_services,
+  $disabled_services,
+)
+{
+
+  ensure_packages( $packages, { 'ensure' => 'present'} )
+
+  ensure_resources('service', $disabled_services, { 'ensure' => 'stopped', 'enable' => false })
+
+  ensure_resources('service', $enabled_services, { 'ensure' => 'running', 'enable' => true })
+
+}
