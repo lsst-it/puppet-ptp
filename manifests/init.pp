3a0acb5140f76cb93d1480f9ea7f488bb4f66195
Bill Glick <bglick@illinois.edu>
2019-10-29 15:35:47 -0500

    Select primary interface for ptp

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,4 +14,15 @@ class ptp (
 
   ensure_resources('service', $services, )
 
+  if $facts['networking']['primary']
+  {
+    $primary_interface = $facts['networking']['primary']
+    file_line { '/etc/sysconfig/ptp4l':
+      notify  => Service['ptp4l'],
+      line    => "OPTIONS=\"-f /etc/ptp4l.conf -i ${primary_interface}\"",
+      match   => '^OPTIONS=',
+      replace_all_matches_not_matching_line => true,
+    }
+  }
+
 }
